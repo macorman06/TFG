4a73ccb598ac3077424bf240a409d80264a10edf
Marcos <48572597+macorman06@users.noreply.github.com>
2025-05-14 18:11:09 +0200

    Updated Welcome.jsx

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -68,11 +68,6 @@ function Welcome({ setView }) {
           description="Ejecuta el modelo matemático en la API de Python y genera soluciones eficientes." 
           onClick={() => setShowPopup("optimize")}
         />
-        <Card 
-          icon={FaChartBar} 
-          title="Comparar Soluciones" 
-          description="Visualiza y analiza diferentes escenarios de planificación para elegir la mejor opción." 
-        />
       </div>
 
       {showPopup === "loadData" && (
